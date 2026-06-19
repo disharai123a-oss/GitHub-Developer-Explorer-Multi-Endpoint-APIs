da6f150e0994cc898dcb1926c2da7d2c6896ce48
Sahil Behera <sahilbehera1012007@gmail.com>
2026-06-19 19:33:29 +0530

    Implement user API request in script.js
    Add main user API request to fetch user data from GitHub.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// Sahil - part 15 - main user API request
+try {
+
+        const userRes =
+            await fetch(
+                `https://api.github.com/users/${username}`
+            );
+
+        const user =
+            await userRes.json();
